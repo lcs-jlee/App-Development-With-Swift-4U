8744ec0ee1999d55448f8eaa312a9794941cd5af
Jeewoo Lee <jlee@lcs.on.ca>
2019-11-05 17:37:14 -0500

    Got 2.3.6 done

--- lab/Lab - Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
+++ lab/Lab - Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
@@ -9,12 +9,29 @@ struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
+    
+    func postWorkoutStats() {
+        print("The distance is \(distance), time is \(time), and elevation is \(elevation)")
+    }
 }
+
+var workOut = RunningWorkout(distance: 2, time: 10, elevation: 20)
+workOut.postWorkoutStats()
+
 /*:
  A `Steps` struct has been created for you below, representing the day's step-tracking data. It has the goal number of steps for the day and the number of steps taken so far. Create a method on `Steps` called `takeStep` that increments the value of `steps` by one. Then create an instance of `Steps` and call `takeStep()`. Print the value of the instance's `steps` property before and after the method call.
  */
 struct Steps { 
     var steps: Int
     var goal: Int
+    
+    mutating func takeStep() {
+        steps += 1
+    }
 }
+
+var firstSteps = Steps(steps: 1, goal: 2)
+print(firstSteps.steps)
+firstSteps.takeStep()
+print(firstSteps.steps)
 //: [Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Computed Properties and Property Observers](@next)
