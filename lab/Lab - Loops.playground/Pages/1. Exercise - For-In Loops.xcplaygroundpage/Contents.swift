d9260c0c52f8c24c37621f948f7d2fcfd2c1ccc3
Jeewoo Lee <jlee@lcs.on.ca>
2020-01-24 20:57:33 -0500

    finish second part of loop

--- lab/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ lab/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -3,17 +3,25 @@
  
  Create a for-in loop that loops through values 1 to 100, and prints each of the values.
  */
-
+for index in 1...100 {
+    print(index)
+}
 
 /*:
  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
  */
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+for letter in alphabet {
+    print(letter)
+}
 
 /*:
  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
  */
-
+var states = ["Washington": "Seatle", "Illinois": "Chicago", "Florida": "Miami"]
+for state in states {
+    print(state)
+}
 
 //: page 1 of 6  |  [Next: App Exercise - Movements](@next)
